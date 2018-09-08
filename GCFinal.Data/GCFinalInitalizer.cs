2f188bf2ad54b4040211dfd56a87e3fb7e7764ef
Chris <butchchr@gmail.com>
2018-09-08 16:31:36 -0400

    Added Fold type and Weight to all objects (still playing with 4 table option)

--- GCFinal.Data/GCFinalInitalizer.cs
+++ GCFinal.Data/GCFinalInitalizer.cs
@@ -7,38 +7,98 @@ namespace GCFinal.Data
     {
         protected override void Seed(GCFinalContext context)
         {
+            //Length, Width and Height are Inches
+            //Weight is Ounces
             context.Clothes.Add(new Clothing()
             {
-                ItemName = "Shirt", //rectangle fold
+                ItemName = "Shirt",
+                FoldType = "Square",
                 Length = 13M,
                 Width = 10M,
-                Height = 0.326M
+                Height = 0.326M,
+                Weight = 8.5M
+            });
+
+            context.Clothes.Add(new Clothing()
+            {
+                ItemName = "Shirt",
+                FoldType = "Rectangle",
+                Length = 6.65M,
+                Width = 10M,
+                Height = 0.715M,
+                Weight = 8.5M
             });
             
             context.Clothes.Add(new Clothing()
             {
-                ItemName = "Pants", //rectangle fold
+                ItemName = "Pants",
+                FoldType = "Rectangle",
                 Length = 19M,
                 Width = 11M,
-                Height = 0.92M
+                Height = 0.92M,
+                Weight = 22.9M
+            });
+
+            context.Clothes.Add(new Clothing(){
+                ItemName = "Pants",
+                FoldType = "Square",
+                Length = 10.5M,
+                Width = 11M,
+                Height = 1.62M,
+                Weight = 22.9M
             });
 
             context.Clothes.Add(new Clothing()
             {
-                ItemName = "Sweater", //rectangle fold
+                ItemName = "Sweater",
+                FoldType  = "Square",
                 Length = 10M,
                 Width = 11.5M,
-                Height = 1.7M
+                Height = 1.7M,
+                Weight = 14M
             });
 
             context.Clothes.Add(new Clothing()
             {
-                ItemName = "Hoodie", //rectangle fold
+                ItemName = "Hoodie",
+                FoldType = "Square",
                 Length = 12M,
                 Width = 11M,
-                Height = 2.675M
+                Height = 2.675M,
+                Weight = 32M
+
             });
 
+            context.Clothes.Add(new Clothing()
+            {
+                ItemName = "Short Socks",
+                FoldType = "Rectangle",
+                Length = 9M,
+                Width = 3.5M,
+                Height = .44M,
+                Weight = 1.2M
+            });
+
+            context.Clothes.Add(new Clothing()
+            {
+                ItemName = "Tall Socks",
+                FoldType = "Rectangle",
+                Length = 9M,
+                Width = 3.5M,
+                Height = 0.72M,
+                Weight = 1.68M
+            });
+
+            context.Clothes.Add(new Clothing()
+            {
+                ItemName = "Undergarments",
+                FoldType = "Rectangle",
+                Length = 4.5M,
+                Width = 7.7M,
+                Height = .45M,
+                Weight = 2.46M
+            });
+            
             context.SaveChanges();
             base.Seed(context);
         }
--- GCFinal.Domain/Models/PackingModels/Clothing.cs
+++ GCFinal.Domain/Models/PackingModels/Clothing.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
         public string ItemName { get; set; }
-        //public string FoldType { get; set; }
+        public string FoldType { get; set; }
         public decimal Length { get; set; }
         public decimal Width { get; set; }
         public decimal Height { get; set; }
-        //public decimal Weight { get; set; }
+        public decimal Weight { get; set; }
     }
 }
