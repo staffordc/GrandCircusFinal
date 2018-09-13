8a511a494b424ca295d1d5796b3acf8a7f0c33df
staffordc <stafford.carh@gmail.com>
2018-09-13 13:17:01 -0400

    Validation Basics

--- GCFinal.MVC/Models/SearchModel.cs
+++ GCFinal.MVC/Models/SearchModel.cs
@@ -1,11 +1,22 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace GCFinal.MVC.Models
 {
     public class SearchModel
     {
+        [Required]
+        [Display(Name = "City, State Abbv. OR Zip Code")]
         public string Location { get; set; }
+        [Required]
+        [DataType(DataType.Date, ErrorMessage = "Please enter a date")]
+        [Display(Name = "Start of Vacation")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime StartDate { get; set; }
+        [Required]
+        [Range(-30,30,ErrorMessage = "Please only plan for a 30d day month long trip")]
+        [Display(Name = "Number of Days Vacationing")]
         public int Duration { get; set; }
+        
     }
 }
