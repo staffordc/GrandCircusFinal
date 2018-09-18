76fbaee542cb83d5b7156a55362e36ec529c3115
Chris Butcher <butchchr@gmail.com>
2018-09-17 23:18:40 -0400

    Refactored Weather Stats Avg to be simple

--- GCFinal.MVC/Controllers/PackingListController.cs
+++ GCFinal.MVC/Controllers/PackingListController.cs
@@ -30,49 +30,48 @@ namespace GCFinal.MVC.Controllers
         public async Task<ActionResult> GetWeatherObject(SearchModel model)
         {
             if (ModelState.IsValid)
-            { 
-            var weatherObject = await _weatherClient.GetHistoricalWeather(model.Location, model.StartDate, model.Duration);
-            var avgPrecipitationMillimeters = decimal.Round((weatherObject.SelectMany(x => x.Hours).Select(x => x.PrecipMm).Sum() / weatherObject.Count), 2, MidpointRounding.AwayFromZero);
-            var avgWindSpeedMph = decimal.Round((weatherObject.SelectMany(x => x.Hours).Select(x => x.WindMph).Sum() / weatherObject.Count / 24), 2, MidpointRounding.AwayFromZero);
-            var avgDailyHighTempF = decimal.Round((weatherObject.Select(x => x.Day).Select(x => x.MaxTempF).Average()), 2, MidpointRounding.AwayFromZero);
-            var avgDailyLowTempF = decimal.Round((weatherObject.Select(x => x.Day).Select(x => x.MinTempF).Average()));
-            var avgDailyAvgTempF = decimal.Round((weatherObject.Select(x => x.Day).Select(x => x.AvgTempF).Average()), 2, MidpointRounding.AwayFromZero);
-            var avgHumidityPercent = decimal.Round((weatherObject.SelectMany(x => x.Hours).Select(x => x.Humidity).Sum() /
-                                                    weatherObject.Count / 24), 2, MidpointRounding.AwayFromZero);
-            var durationDeciaml = Convert.ToDecimal(model.Duration);
-            var itemsToPack =
-                _tripPackingService.PackItems(avgDailyAvgTempF, avgPrecipitationMillimeters, avgWindSpeedMph, durationDeciaml).ToList();
-            List<Container> containers = new List<Container>();
-            containers.Add(new Container(1, "Carry-On", 20.5M, 15M, 8M)); //samsonite 21" Spinner
-            containers.Add(new Container(2, "Medium Suitcase", 27M, 18.5M, 9.5M)); //samsonite 27" Spinner (27M, 18.5M,9.5M)
-            containers.Add(new Container(3, "Large Suitcase", 33.5M, 22M, 11M));
-            List<Item> itemsToContainer = new List<Item>();
-            foreach (var item in itemsToPack)
             {
-                itemsToContainer.Add(new Item(item.Name, item.Height, item.Length, item.Width, Convert.ToInt32(item.Quantity)));
-            }
-            List<int> algorithms = new List<int>();
-            algorithms.Add((int)AlgorithmType.EB_AFIT);
-            List<ContainerPackingResult> packingResults = new List<ContainerPackingResult>();
-            var vm = new WeatherViewModel();
-            for (int i = containers.Count - 1; i >= 0; i--)
-            {
-                packingResults = PackingService.Pack(containers[i], itemsToContainer, algorithms);
-                var packResult = packingResults.SelectMany(x => x.AlgorithmPackingResults).SelectMany(x => x.UnpackedItems).Count();
-                if (packResult == 0)
+                var weatherObject = await _weatherClient.GetHistoricalWeather(model.Location, model.StartDate, model.Duration);
+                var hourlies = weatherObject.SelectMany(x => x.Hours).ToList();
+                var dailies = weatherObject.Select(x => x.Day).ToList();
+                var countOfDailies = dailies.Count();
+                var avgPrecipitationMillimeters = hourlies.Sum(x => x.PrecipMm) / countOfDailies;
+                var avgWindSpeedMph = hourlies.Average(x => x.WindMph);
+                var avgDailyHighTempF = dailies.Average(x => x.MaxTempF);
+                var avgDailyLowTempF = dailies.Average(x => x.MinTempF);
+                var avgDailyAvgTempF = dailies.Average(x => x.AvgTempF);
+                var avgHumidityPercent = hourlies.Average(x => x.Humidity);
+                var itemsToPack = this._tripPackingService.PackItems(avgDailyAvgTempF, avgPrecipitationMillimeters, avgWindSpeedMph, Convert.ToDecimal(model.Duration)).ToList(); List<Container> containers = new List<Container>();
+                containers.Add(new Container(1, "Carry-On", 20.5M, 15M, 8M)); //samsonite 21" Spinner
+                containers.Add(new Container(2, "Medium Suitcase", 27M, 18.5M, 9.5M)); //samsonite 27" Spinner (27M, 18.5M,9.5M)
+                containers.Add(new Container(3, "Large Suitcase", 33.5M, 22M, 11M));
+                List<Item> itemsToContainer = new List<Item>();
+                foreach (var item in itemsToPack)
                 {
-                    vm.AvgPrecip = avgPrecipitationMillimeters;
-                    vm.AvgWind = avgWindSpeedMph;
-                    vm.DailyMaxTemp = avgDailyHighTempF;
-                    vm.DailyMinTemp = avgDailyLowTempF;
-                    vm.DailyAvgTemp = avgDailyAvgTempF;
-                    vm.AvgHumidity = avgHumidityPercent;
-                    vm.PackingItems = itemsToPack;
-                    vm.ContainerPackingResults = packingResults;
+                    itemsToContainer.Add(new Item(item.Name, item.Height, item.Length, item.Width, Convert.ToInt32(item.Quantity)));
                 }
-            }
+                List<int> algorithms = new List<int>();
+                algorithms.Add((int)AlgorithmType.EB_AFIT);
+                List<ContainerPackingResult> packingResults = new List<ContainerPackingResult>();
+                var vm = new WeatherViewModel();
+                for (int i = containers.Count - 1; i >= 0; i--)
+                {
+                    packingResults = PackingService.Pack(containers[i], itemsToContainer, algorithms);
+                    var packResult = packingResults.SelectMany(x => x.AlgorithmPackingResults).SelectMany(x => x.UnpackedItems).Count();
+                    if (packResult == 0)
+                    {
+                        vm.AvgPrecip = avgPrecipitationMillimeters;
+                        vm.AvgWind = avgWindSpeedMph;
+                        vm.DailyMaxTemp = avgDailyHighTempF;
+                        vm.DailyMinTemp = avgDailyLowTempF;
+                        vm.DailyAvgTemp = avgDailyAvgTempF;
+                        vm.AvgHumidity = avgHumidityPercent;
+                        vm.PackingItems = itemsToPack;
+                        vm.ContainerPackingResults = packingResults;
+                    }
+                }
 
-            return View("Result", vm);
+                return View("Result", vm);
             }
 
             return RedirectToAction("Index", "Home");
