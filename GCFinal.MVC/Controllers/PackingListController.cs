514ef3bda46018e170b76ff04b5cba6b24a38103
sean <armstrong.d.sean@gmail.com>
2018-09-20 21:16:53 -0400

    dev fix: noticed medium suitcase was off on last measurement- was 99, not 9

--- GCFinal.MVC/Controllers/PackingListController.cs
+++ GCFinal.MVC/Controllers/PackingListController.cs
@@ -103,7 +103,7 @@ namespace GCFinal.MVC.Controllers
 
                 List<Container> containers = new List<Container>();
                 containers.Add(new Container(1, "Carry-On", 104M, 20.5M, 15M, 8M)); //samsonite 21" Spinner - 43.5 total
-                containers.Add(new Container(2, "Medium Suitcase", 144.88M, 23M, 17M, 99M)); //samsonite 27" Spinner (27M, 18.5M,9.5M) - 55 total
+                containers.Add(new Container(2, "Medium Suitcase", 144.88M, 23M, 17M, 9M)); //samsonite 27" Spinner (27M, 18.5M,9.5M) - 55 total
                 containers.Add(new Container(3, "Large Suitcase", 145.6M, 29.5M, 20.5M, 11M)); //62" (must be 62" and 50 lbs or less)
                 List<Item> itemsToContainer = new List<Item>();
 
